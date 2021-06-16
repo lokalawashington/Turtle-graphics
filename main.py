a1868c2bfa557e0c28619833da0d3f65e4523597
peter ngotho <sammykamau772@mail.com>
2021-06-16 11:18:23 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+"""https://docs.python.org/3/library/turtle.html"""
+"""https://cs111.wellesley.edu/labs/lab01/colors"""
+"""https://trinket.io/docs/colors"""
+from turtle import Turtle, Screen
+
+timmy_the_turtle = Turtle()
+"""turtle graphics doc"""
+timmy_the_turtle.shape("turtle")
+
+screen = Screen()
+screen.exitonclick()
